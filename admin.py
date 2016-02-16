7b2b4ead5f5852cd604d696a5fe84cf1ce93ebec
muhammadnash <m.nashaat3@gmail.com>
2016-02-16 13:52:27 +0200

    Registering models in admin.

--- admin.py
+++ admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models.subject import Subject
+from .models.document import Document
+from .models.page import Page
+from .models.comment import  DocumentComment , PageComment
+
 # Register your models here.
+
+
+admin.site.register(Subject)
+admin.site.register(Document)
+admin.site.register(Page)
+admin.site.register(DocumentComment)
+admin.site.register(PageComment)
+
+
