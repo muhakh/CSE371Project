1a788b2ca1a54c021c9e5d01aa8ffce5992b2be7
muhammadnash <m.nashaat3@gmail.com>
2016-02-16 13:06:42 +0200

    Created class views of subject model 0.00

---
+++ views/subject.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import CreateView, UpdateView, DeleteView ,ListView
+from models.subject import Subject
+
+class CreateSubject(CreateView):
+    model = Subject
+
+class DeleteSubject(DeleteView):
+    model = Subject
+
+class ListSubject(ListView):
+    model = Subject
+
+class UpdateSubject(UpdateView):
+    model = Subject
