e3b61fccebbcfe20e790ff9970839659171ccf72
muhammadnash <m.nashaat3@gmail.com>
2016-02-16 14:35:33 +0200

    configuring views path

--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
-import subject
-import document
-import page
-import comment
+from ..models.subject import Subject
+from ..models.document import Document
+from ..models.page import Page
+from ..models.comment import DocumentComment , PageComment
