d163494cb8adc38b714cc79dcc6cdc2b8dcd2239
muhakh <muhakh2@gmail.com>
2016-02-16 12:46:50 +0200

    Ignore added

--- models/comment.py
+++ models/comment.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .document import Document
+from .page import Page
 
 class Comment(models.Model):
-    pass
+    content = models.TextField()
+    owner = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+        app_label = "gallery"
+    def __str__(self):
+        return self.content
 
 class DocumentComment(models.Model):
-    pass
+    document = models.ForeignKey(Document)
 
 class PageComment(models.Model):
-    pass
+    page = models.ForeignKey(Page)
