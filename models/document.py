00bc2a15ade123ce9f9e1cf9ed91011a2ef661b0
muhakh <muhakh2@gmail.com>
2016-02-18 10:47:03 +0200

    Fixing some issues with document model

--- models/document.py
+++ models/document.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
-
+from .subject import Subject
 # Create your models here.
 class Document(models.Model):
-	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses_created')
-	subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='courses')
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='doucument_owner')
+	subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='doucument_subject')
 	title = models.CharField(max_length=200)
 	slug = models.SlugField(max_length=200, unique=True)
 	file = models.FileField(upload_to='')
