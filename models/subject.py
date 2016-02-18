526a329bff09d4038faaa8a8737f6da6f1f15c3f
muhakh <muhakh2@gmail.com>
2016-02-18 15:52:36 +0200

    Edited subject model to add slugify

--- models/subject.py
+++ models/subject.py
@@ -1,11 +1,16 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     def __str__(self):
         return self.title
     class Meta:
-        pass
-
+        ordering = ('title',)
+    def save(self):
+        super(Subject, self).save()
+    	self.slug = slugify(self.title)
+    	super(Subject, self).save()
+    def get_absolute_url(self):
+        return "/subject/view/%s/" % self.slug
