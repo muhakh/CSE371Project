0361f9ad9e51860e785430ffd818b5b7efe164d1
muhakh <muhakh2@gmail.com>
2016-02-18 20:29:41 +0200

    Removed Page list view

--- views/page.py
+++ views/page.py
@@ -1,10 +1,6 @@
 from django.views.generic import ListView , DetailView
-from courses.models import Page
-
-class PageList(ListView):
-    model=Page
-    template_name='page_view_1.html'
+from ..models.page import Page
 
 class PageDetail(DetailView):
     model=Page
-    template_name='page_view_2.html'
+    template_name='page/detail.html'
