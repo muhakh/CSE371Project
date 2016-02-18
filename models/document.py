44a1c507f873353628006504e455e005b6e1ed67
muhakh <muhakh2@gmail.com>
2016-02-18 15:54:57 +0200

    Fixing some issues with document model

--- models/document.py
+++ models/document.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 from .subject import Subject
+from django.template.defaultfilters import slugify
 
 class Document(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='doucument_owner')
@@ -12,5 +13,10 @@ class Document(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	class Meta:
 		ordering = ('-created',)
+	def save(self):
+		super(Document, self).save()
+		self.slug = slugify(self.title)
+		super(Document, self).save()
+
 	def __str__(self):
 		return self.title
