f52fe8937b69ee11a85cb2bbe264430f7583b136
muhakh <muhakh2@gmail.com>
2016-02-18 15:56:13 +0200

    Fixing some issues with subject view

--- views/subject.py
+++ views/subject.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import CreateView, UpdateView, DeleteView ,ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import CreateView, UpdateView, DeleteView ,ListView, DetailView
+from django.shortcuts import get_object_or_404
 from ..models.subject import Subject
+from ..models.document import Document
 
 class CreateSubject(CreateView):
     model = Subject
-
+    fields = ('title',)
+    template_name = 'subject/form.html'
 
 class DeleteSubject(DeleteView):
     model = Subject
 
-class ListSubject(ListView):
+class DetailSubject(LoginRequiredMixin, DetailView):
     model = Subject
+    template_name = 'subject/detail.html'
+    def documents(self):
+        return Document.objects.filter(subject_id = self.kwargs['pk'])
+
+class ListSubject(LoginRequiredMixin, ListView):
+    model = Subject
+    template_name = 'subject/list.html'
 
 class UpdateSubject(UpdateView):
     model = Subject
-
+    fields = ('title',)
+    template_name = 'subject/form.html'
