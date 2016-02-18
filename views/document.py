1ba0b790e5a8e65d618b45e60ede9c3ac2f7dcf1
muhakh <muhakh2@gmail.com>
2016-02-18 15:55:58 +0200

    Fixing some issues with document view

--- views/document.py
+++ views/document.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from ..models.document import Document
-# Create your views here.
+from ..models.page import Page
 
 class OwnerMixin(object):
 	def get_queryset(self):
@@ -20,7 +20,7 @@ class OwnerDocumentMixin(OwnerMixin, LoginRequiredMixin):
 	model = Document
 
 class OwnerDocumentEditMixin(OwnerDocumentMixin, OwnerEditMixin):
-	fields = ['title', 'slug','file', ]		#you've to add subject field here
+	fields = ['title', 'file', 'subject']
 	success_url = reverse_lazy('manage_document_list')
 	template_name = 'document/form.html'
 
@@ -28,12 +28,18 @@ class ManageDocumentListView(OwnerDocumentMixin, ListView):
 	template_name = 'document/list.html'
 
 class CreateDocumentView(PermissionRequiredMixin, OwnerDocumentEditMixin, CreateView):
-	permission_required = 'CSE371Projectadd_document'
+	permission_required = 'CSE371Project.add_document'
 
 class DocumentUpdateView(PermissionRequiredMixin, OwnerDocumentEditMixin, UpdateView):
-	permission_required = 'CSE371Projectchange_document'
+	permission_required = 'CSE371Project.change_document'
 
 class DocumentDeleteView(PermissionRequiredMixin, OwnerDocumentMixin, DeleteView):
 	template_name = 'document/delete.html'
 	success_url = reverse_lazy('manage_document_list')
-	permission_required = 'CSE371Projectdelete_document'
+	permission_required = 'CSE371Project.delete_document'
+
+class DocumentDetailView(LoginRequiredMixin, DetailView):
+	model = Document
+	template_name = 'document/detail.html'
+	def pages(self):
+		return Page.objects.filter(document = self.kwargs['pk'])
